581ca715633706fad48ace62aad799028551a4d8
BluePudding0913 <umi2003256@gmail.com>
2026-07-11 19:22:09 +0900

    jsonに"_"の文章を追加することで、1つ前の文章を消さないよう設定可能に

--- script.js
+++ script.js
@@ -10,25 +10,18 @@ fetch('texts.json')
 // ボタンクリック時の処理
 document.getElementById('go').addEventListener('click', () => {
   if (currentIndex < messages.length) {
+    let flg = false;
+    if((messages[currentIndex]=="_"))flg = true;
     const container = document.getElementById('text-area');
-    
-    //表示するメッセージが３つ以上なら、最初のメッセージを削除する
-    if (currentIndex > 1) {
-      container.removeChild(container.firstChild);
-    }
+    //メッセージを削除する
+    if (currentIndex > 0 && !flg)container.replaceChildren();
 
-
-
-    // ★ 'a' や 'div' ではなく 'span' にする
+    if(flg)currentIndex++;
     const newText = document.createElement('span'); 
-    
     newText.classList.add('fade-t'); 
     newText.classList.add('txt'); 
-    
-    // 句点の後などに少し隙間が欲しい場合はスペースを足すのもアリです
-    newText.textContent = messages[currentIndex] + " "; 
-    
-    container.appendChild(newText); 
+    newText.textContent = messages[currentIndex];
+    container.appendChild(newText);
     currentIndex++; 
   } else {
     alert('すべてのメッセージを表示しました！');
